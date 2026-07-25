7c574ad8ce738f74e0da12ca355c9e8872ea0888
Arne Hassel <arne.hassel@gmail.com>
2026-07-25 16:42:56 +0200

    Fix layout for DDG mobile

--- src/_includes/layout.tsx
+++ src/_includes/layout.tsx
@@ -23,29 +23,31 @@ export const Layout: FC<Props> = ({ breadcrumbs, children, title }) => {
 				<link rel="stylesheet" href="/css/style.css" />
 			</head>
 			<body>
-				<header>
-					<h1 className="title">{title || BASE_TITLE}</h1>
-					{breadcrumbs && (
-						<nav className="breadcrumb" aria-label="Breadcrumbs">
-							<ul>
-								{breadcrumbs.map(({ url, title }) => (
-									<li key={`nav-${title}`}>
-										<a href={url}>{title}</a>
-									</li>
-								))}
-							</ul>
-						</nav>
-					)}
-				</header>
-				<main>{children}</main>
-				<footer>
-					This site is licensed with{" "}
-					<a href="https://creativecommons.org/licenses/by-nc/4.0/">
-						CC BY-NC 4.0
-					</a>
-					. Code available at{" "}
-					<a href="https://github.com/megoth/icanhasweb-11ty">GitHub</a>.
-				</footer>
+				<div className="layout-container">
+					<header>
+						<h1 className="title">{title || BASE_TITLE}</h1>
+						{breadcrumbs && (
+							<nav className="breadcrumb" aria-label="Breadcrumbs">
+								<ul>
+									{breadcrumbs.map(({ url, title }) => (
+										<li key={`nav-${title}`}>
+											<a href={url}>{title}</a>
+										</li>
+									))}
+								</ul>
+							</nav>
+						)}
+					</header>
+					<main>{children}</main>
+					<footer>
+						This site is licensed with{" "}
+						<a href="https://creativecommons.org/licenses/by-nc/4.0/">
+							CC BY-NC 4.0
+						</a>
+						. Code available at{" "}
+						<a href="https://github.com/megoth/icanhasweb-11ty">GitHub</a>.
+					</footer>
+				</div>
 			</body>
 		</html>
 	);
