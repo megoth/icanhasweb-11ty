5cb55f1c3f976870ea13d3f4752187d8886e07c9
Arne Hassel <arne.hassel@gmail.com>
2026-07-25 16:46:16 +0200

    Revert "Fix layout for DDG mobile"
    This reverts commit 7c574ad8ce738f74e0da12ca355c9e8872ea0888.

--- src/_includes/layout.tsx
+++ src/_includes/layout.tsx
@@ -23,31 +23,29 @@ export const Layout: FC<Props> = ({ breadcrumbs, children, title }) => {
 				<link rel="stylesheet" href="/css/style.css" />
 			</head>
 			<body>
-				<div className="layout-container">
-					<header>
-						<h1 className="title">{title || BASE_TITLE}</h1>
-						{breadcrumbs && (
-							<nav className="breadcrumb" aria-label="Breadcrumbs">
-								<ul>
-									{breadcrumbs.map(({ url, title }) => (
-										<li key={`nav-${title}`}>
-											<a href={url}>{title}</a>
-										</li>
-									))}
-								</ul>
-							</nav>
-						)}
-					</header>
-					<main>{children}</main>
-					<footer>
-						This site is licensed with{" "}
-						<a href="https://creativecommons.org/licenses/by-nc/4.0/">
-							CC BY-NC 4.0
-						</a>
-						. Code available at{" "}
-						<a href="https://github.com/megoth/icanhasweb-11ty">GitHub</a>.
-					</footer>
-				</div>
+				<header>
+					<h1 className="title">{title || BASE_TITLE}</h1>
+					{breadcrumbs && (
+						<nav className="breadcrumb" aria-label="Breadcrumbs">
+							<ul>
+								{breadcrumbs.map(({ url, title }) => (
+									<li key={`nav-${title}`}>
+										<a href={url}>{title}</a>
+									</li>
+								))}
+							</ul>
+						</nav>
+					)}
+				</header>
+				<main>{children}</main>
+				<footer>
+					This site is licensed with{" "}
+					<a href="https://creativecommons.org/licenses/by-nc/4.0/">
+						CC BY-NC 4.0
+					</a>
+					. Code available at{" "}
+					<a href="https://github.com/megoth/icanhasweb-11ty">GitHub</a>.
+				</footer>
 			</body>
 		</html>
 	);
